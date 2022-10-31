7559aa269cea0d4c1ecf6079f343f39657376c8f
elise <115508722+cultelise@users.noreply.github.com>
2022-10-30 22:26:28 -0500

    Add remove button function and styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,37 @@
-const tableHeader = document.getElementsByName('th');
-const myLibrary = [];
-let output = document.getElementById('output');
+const tableHeader = document.getElementsByName('th'),
+      output = document.getElementById('output'),
+      tableBody = document.getElementById('tableBody'),
+      myLibrary = [],
+      rem = document.getElementById('rem');
 
-
-
-function Book(title, author, pages, date, rating) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.date = date,
-  this.rating = rating;
+class Book {
+  constructor(title, author, pages, date, rating) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.date = date,
+    this.rating = rating;
+  }
 }
 
-
 function addBookToLibrary() {
-  let info = document.querySelector('#book').value.split(' ');
-  let bookInfo = new Book(info[0], info[1], info[2], info[3], info[4])
+  const info = document.querySelector('#book').value.split(' '),
+        bookInfo = new Book(info[0], info[1], info[2], info[3], info[4]);
   myLibrary.push(bookInfo);
   displayLibrary();
+}
+
+function removeButton() {
+  const removeBook = document.createElement('button'), 
+        buttonWord = document.createTextNode("Remove Book");
+  removeBook.appendChild(buttonWord);
+  rem.appendChild(removeBook);
 }
 
 function displayLibrary() {
   for (let book of myLibrary) {
     if (myLibrary[myLibrary.length - 1] === book) {
-      let tableBody = document.getElementById('bleh'),
-          bodyRow = document.createElement('tr');
+      const bodyRow = document.createElement('tr');
       for (let detail in book) {
         let node, data;
         switch (detail) {
@@ -60,12 +67,7 @@ function displayLibrary() {
             break;
         }
       } tableBody.appendChild(bodyRow);
-        let removeBook = document.createElement('button');
-        let buttonWord = document.createTextNode("Remove");
-        removeBook.appendChild(buttonWord);
-        console.log(output, removeBook)
-        output.appendChild(removeBook);
-        output.style.display = 'flex';
+        removeButton();
     }
   }
 }
